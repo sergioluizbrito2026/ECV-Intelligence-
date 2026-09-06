acbd044cbe01ed217f0ec32a03ba6936eff48b27
Sergio Luiz Brito <sergiolmendes2026@gmail.com>
2026-09-06 20:11:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1492,7 +1492,7 @@ elif page == "IA & Insights":
         # ----------------------------------------------------
 
         
-       if "tipo_vistoria" in df.columns:
+              if "tipo_vistoria" in df.columns:
 
             tipos = (
                 df["tipo_vistoria"]
@@ -1503,7 +1503,7 @@ elif page == "IA & Insights":
 
             tipos = tipos[tipos != ""]
 
-         if not tipos.empty:
+            if not tipos.empty:
 
                 contagem_tipos = tipos.value_counts()
 
@@ -1542,15 +1542,7 @@ elif page == "IA & Insights":
 """,
                 unsafe_allow_html=True,
             )
-            st.markdown(
-                f"""
-<div class="card">
-{insight}
-</div>
-""",
-                unsafe_allow_html=True,
-            )
-```
+
 
     # ========================================================
     # ANÁLISE DE PERFORMANCE
